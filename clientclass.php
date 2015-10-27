0e16bdf09e2c6e8748e8240eb12411aa0f7e4e95
Wainaina3 <dwainaina3@gmail.com>
2015-10-27 03:27:02 +0000

    My Second commit

--- clientclass.php
+++ clientclass.php
@@ -1,1 +1,22 @@
+<?php
+include_once('base.php');
 
+class clientclass extends base{
+
+    //function to fetch all data from the dabatabase
+
+    function fetchAll()
+    {
+        $sql="select * from news order by TIMESTAMP";
+
+        return $this->query($sql);
+    }
+
+    function fetchNewsByFilter($filter)
+    {
+        $sql="select * from news where category='$filter'";
+
+        return $this->query($sql);
+    }
+}
+?>
--- controller.php
+++ controller.php
@@ -1,1 +1,61 @@
+<?php
+if (isset($_REQUEST['cmd'])){
 
+    $cmd=$_REQUEST['cmd'];
+
+    function getNews(){
+        include_once("clientclass.php");
+        $news=new clientclass();
+
+        $news->fetchAll();
+        if($row=$news->fetchArray()){
+            echo '{"result":1,news:[';
+            while($row){
+                echo json_encode($row);
+                if($row=$news->fetchArray()){
+                    echo ',';
+                }
+            }
+
+            echo ']}';
+        }
+        echo '{"result":0,"message":"No news found"}';
+    }
+
+    //get news by filter
+     function getNewsByFilters()
+    {
+        include_once("clientclass.php");
+        $news=new clientclass();
+        $filter=$_REQUEST['filter'];
+        $news->fetchNewsByFilter(filter);
+        if($row=$news->fetchArray()){
+            echo '{"result":1,news:[';
+            while($row){
+                echo json_encode($row);
+                if($row=$news->fetchArray()){
+                    echo ',';
+                }
+            }
+
+            echo ']}';
+        }
+        echo '{"result":0,"message":"No news found"}';
+    }
+
+    switch ($cmd) {
+        case 1:
+            getNews();
+            break;
+        case 2:
+            getNewsByFilters();
+            break;
+
+        default:
+
+            break;
+    }
+
+
+}
+?>
